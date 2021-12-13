76e7edaad5e87b2be01eb703e7c17bab5b8c752d
Morten Midtgaard <morten.midtgaard@ARRESTUNABLEARMY.workdayinternal.com>
2021-12-13 09:36:14 +0100

    added function to write to Excel

--- stock_scraper.py
+++ stock_scraper.py
@@ -1,8 +1,14 @@
-#https://medium.com/c%C3%B3digo-ecuador/how-to-scrape-yahoo-stock-price-history-with-python-b3612a64bdc6
+#https://blog.quantinsti.com/stock-market-data-analysis-python/
+import yfinance as yf
+import xlsxwriter as xl
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
-page  = requests.get("https://www.fool.com/investing/top-stocks-to-buy.aspx")
+start_date = '2020-03-01'
+end_date = '2021-12-09'
 
-print(page)
+# Setting tickers
+tickers = 'AMZN'
+
+data = yf.download(tickers, start_date, end_date)
+
+data.to_excel('example1.xlsx')
