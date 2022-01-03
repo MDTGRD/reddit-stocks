104bf741bf7579848e58fda77e2d9b9e2a90be97
Morten Midtgaard <morten.midtgaard@ARRESTUNABLEARMY.workdayinternal.com>
2022-01-03 16:12:44 +0100

    Created functionality to visualize the ticker trend

--- stock_scraper.py
+++ stock_scraper.py
@@ -3,12 +3,32 @@ import yfinance as yf
 import xlsxwriter as xl
 import pandas as pd
 
-start_date = '2020-03-01'
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+start_date = '2021-10-01'
 end_date = '2021-12-09'
 
 # Setting tickers
 tickers = 'AMZN'
 
+# Downloading data
 data = yf.download(tickers, start_date, end_date)
 
-data.to_excel('example1.xlsx')
+# Wrangling Data
+data = data[['Adj Close']].round(2)
+data = data.reset_index()
+print(data)
+
+# Visualization
+# sns.set_theme()
+
+sns.lineplot(data=data,
+    x='Date', y='Adj Close'
+)
+
+plt.show()
+
+
+# Writing to Excel
+#data.to_excel('stock_overview.xlsx')
